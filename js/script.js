36fea66215fa1bce1101f1071609feaa51f8d7a7
Ibraima Camara <ibojuniorcamara1@gmail.com>
2026-06-03 10:38:24 +0100

    Liga formulário de alunos à base de dados

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const formularioAluno = document.querySelector(".formulario-aluno");
-    const mensagemAluno = document.getElementById("mensagemAluno");
-
-    if (formularioAluno) {
-        formularioAluno.addEventListener("submit", function (evento) {
-            evento.preventDefault();
-
-            mensagemAluno.textContent = "Validação efetuada com sucesso. O aluno será guardado quando ligarmos o PHP à base de dados.";
-        });
-    }
-});
+dconst formularioAluno = document.querySelector(".formulario-aluno");
 const formularioProfessor = document.querySelector(".formulario-professor");
 const mensagemProfessor = document.getElementById("mensagemProfessor");
 
